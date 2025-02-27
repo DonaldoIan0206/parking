49ee18c82ebf5e529311f563b1d3bc141a85a26e
Donaldo Ian <lopezzacauladonaldoian290@gmail.com>
2025-02-27 12:40:11 -0600

    Configuracion V1

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>Hola desde React en Laravel</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ use App\Http\Controllers\ParkingSpotController;
 Route::get('/parking-spots', [ParkingSpotController::class, 'index']);
 Route::post('/parking-spots', [ParkingSpotController::class, 'store']);
 Route::put('/parking-spots/{id}', [ParkingSpotController::class, 'update']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
